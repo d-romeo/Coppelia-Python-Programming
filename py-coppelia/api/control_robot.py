9b4cb6b7c3eb43fd222356470364652e3671706e
Daniele Romeo <danieleromeo.ele@gmail.com>
2023-10-07 12:17:23 +0200

    jointHandles

--- py-coppelia/api/control_robot.py
+++ py-coppelia/api/control_robot.py
@@ -7,7 +7,7 @@ import numpy
 #create connection 
 print("Program Started")
 sim.simxFinish(-1)
-clientID = sim.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
 
 #check connection
 if(clientID != -1):
@@ -16,14 +16,18 @@ else:
     sys.exit('Failed To connect.')
 time.sleep(1)
 
-
 # handles
-jointHandles={-1,-1,-1,-1,-1,-1,-1}
-for i in range(7):        
-    jointHandles[i]=sim.simxGetObjectHandle('./joint', 0)
+jointHandles=[-1,-1,-1,-1,-1,-1,-1]
 
+error_code,jointHandles[0]=sim.simxGetObjectHandle(clientID, '/Franka/joint' ,sim.simx_opmode_oneshot_wait) 
+error_code,jointHandles[1]=sim.simxGetObjectHandle(clientID, '/Franka/link2_resp/joint' ,sim.simx_opmode_oneshot_wait)
+error_code,jointHandles[2]=sim.simxGetObjectHandle(clientID, '/Franka/link3_resp/joint' ,sim.simx_opmode_oneshot_wait)
+error_code,jointHandles[3]=sim.simxGetObjectHandle(clientID, '/Franka/link4_resp/joint' ,sim.simx_opmode_oneshot_wait)
+error_code,jointHandles[4]=sim.simxGetObjectHandle(clientID, '/Franka/link5_resp/joint' ,sim.simx_opmode_oneshot_wait)
+error_code,jointHandles[5]=sim.simxGetObjectHandle(clientID, '/Franka/link6_resp/joint' ,sim.simx_opmode_oneshot_wait)
+error_code,jointHandles[6]=sim.simxGetObjectHandle(clientID, '/Franka/link7_resp/joint' ,sim.simx_opmode_oneshot_wait)
 
-
+print(jointHandles) 
 
 
 
--- py-coppelia/api/test_connection.py
+++ py-coppelia/api/test_connection.py
@@ -13,7 +13,7 @@ import time
 
 print ('Program started')
 sim.simxFinish(-1) # just in case, close all opened connections
-clientID=sim.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to CoppeliaSim
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
 if clientID!=-1:
     print ('Connected to remote API server')
 
